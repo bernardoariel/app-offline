7147a32d5f36d088d52edbea6da217b190bbb5a4
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-02-21 19:02:03 -0300

    Agregando cosas

--- src/helpers/getDropItems.ts
+++ src/helpers/getDropItems.ts
@@ -1,6 +1,7 @@
 import { afectados, documentos, sexo, nacionalidad, estadoCivil, instruccion } from "@/data/actuacionNew";
 import { ref } from "vue";
 import { mapToDropdownItems } from "./dropUtils";
+import { departamentosDropdown } from "@/data/municipios";
 
 export const afectadosDropdown = ref(mapToDropdownItems(afectados));
 export const documentosDropdown = ref(mapToDropdownItems(documentos));
@@ -8,3 +9,4 @@ export const sexoDropdown = ref(mapToDropdownItems(sexo));
 export const nacionalidadDropdown = ref(mapToDropdownItems(nacionalidad));
 export const estadoCivilDropdown = ref(mapToDropdownItems(estadoCivil));
 export const instruccionDropdown = ref(mapToDropdownItems(instruccion));
+export const municipiosDropdown = ref(mapToDropdownItems(departamentosDropdown));
--- src/interfaces/fecha.interface.ts
+++ src/interfaces/fecha.interface.ts
@@ -2,3 +2,15 @@ export interface Fecha{
     titulo: string,
     valor: string | null
 }
+
+export interface FechaUbicacion{
+    desdeFechaHora: string | Date,
+    hastaFechaHora: string | Date,
+    calle?: string,
+    numero?: string,
+    departamento?: string,
+    /* si hay internet */
+    ubicacion: string,
+    latitud?: string,
+    longitud?: string
+}

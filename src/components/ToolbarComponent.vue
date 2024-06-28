50ae604dac4a1add1fc1f42afea3ffc6485fabcc
GabrielNavas98 <cgnavas98@gmail.com>
2024-06-28 17:12:48 -0300

    Feat: my calendar element

--- src/components/ToolbarComponent.vue
+++ src/components/ToolbarComponent.vue
@@ -1,41 +1,36 @@
 <script lang="ts" setup>
-import SidebarMenu from './SidebarMenu.vue';
-import SidebarRight from './SidebarRight.vue';
-import useActuacion from '../composables/useActuacion';
-import { onMounted } from 'vue';
+  import SidebarMenu from './SidebarMenu.vue';
+  import SidebarRight from './SidebarRight.vue';
+  import useActuacion from '../composables/useActuacion';
+  import MyCalendar from './elementos/MyCalendar.vue'
+  import { ref, onMounted } from 'vue';
 
-const {fechaCreacion,toogleDateActuacion,refreshDate} = useActuacion()
-onMounted(()=>toogleDateActuacion())
+  const { fechaCreacion, toogleDateActuacion } = useActuacion();
+  const showCalendar = ref(false);
 
-const toogleDate = () =>{
-  refreshDate()
-}
+  onMounted(() => toogleDateActuacion());
+
+  const toggleCalendar = () => {
+    showCalendar.value = !showCalendar.value;
+  };
+  const today = ref(fechaCreacion);
 </script>
+
 <template>
-     <Toolbar>
-      <template #start>
-        <SidebarMenu  icono="pi-microsoft" position="left" color-icono="primary" />
-      </template>
+  <Toolbar>
+    <template #start>
+      <SidebarMenu icono="pi-microsoft" position="left" color-icono="primary" />
+    </template>
 
-      <template #center>
-       
-        <div v-if="fechaCreacion"  class="flex align-items-center ">
-          <div class="text-3xl"> Denuncia en fecha:</div>
-          <div class="text-3xl ml-5">{{ fechaCreacion }}</div>
-          <div class="flex items-center ml-3">
-            <Button icon="pi pi-calendar" severity="primary" text rounded />
-            <Button @click="toogleDate()"  icon="pi pi-refresh" severity="primary" text rounded />
-          </div>
-        </div>
-        <div v-else class="p-d-flex p-flex-column text-3xl">Actuaciones</div>
-     
-      </template>
-
+    <template #center>
+      <div v-if="fechaCreacion" class="flex align-items-center gap-2">
+        <div class="text-3xl">Actuación en fecha:</div>
+        <MyCalendar v-model="today" @close="toggleCalendar" />
+      </div>
+      <div v-else class="p-d-flex p-flex-column text-3xl">Actuaciones</div>
+    </template>
     <template #end>
-        <SidebarRight  icono="pi-bars" position="right" color-icono="secondary"/>
-      </template>
-    </Toolbar>
+      <SidebarRight icono="pi-bars" position="right" color-icono="secondary" />
+    </template>
+  </Toolbar>
 </template>
-
-
-
--- src/composables/useActuacion.ts
+++ src/composables/useActuacion.ts
@@ -2,31 +2,31 @@ import { ref } from "vue"
 import type { Actuacion } from '@/interfaces/tipoActuaciones.interface';
 import { useRoute } from 'vue-router';
 import { useRouter } from 'vue-router';
-import  { actuaciones } from '@/data/tipoActuaciones';
+import { actuaciones } from '@/data/tipoActuaciones';
 import { formatFecha } from '../helpers/getFormatFecha';
 
 let fechaCreacion = ref(formatFecha(new Date()))
 
 const useActuacion = () => {
-    
+
     const router = useRouter();
     const route = useRoute();
-    const toogleDateActuacion = () =>{
+    const toogleDateActuacion = () => {
 
         if (fechaCreacion.value && route.name !== 'actuacion') {
             console.log('es null')
             fechaCreacion.value = null
 
-        }else{
+        } else {
             fechaCreacion.value = formatFecha(new Date())
             console.log('no es null')
-        }   
+        }
     }
-    const refreshDate = ()=>{
+    const refreshDate = () => {
         fechaCreacion.value = formatFecha(new Date())
     }
     const agregarNuevoItem = (key: string) => {
-       router.push({ name: 'formulario', params: { tipo: key } });
+        router.push({ name: 'formulario', params: { tipo: key } });
     };
 
     return {

d5ecc8a1d37092acbf2a496e317734a165c48b6f
GabrielNavas98 <cgnavas98@gmail.com>
2024-07-02 10:20:25 -0300

    Review, corrección del switch del modooscuro, y estetica de botones

--- src/components/SetColorSystem.vue
+++ src/components/SetColorSystem.vue
@@ -1,30 +1,42 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 import { usePrimeVue } from 'primevue/config';
 import useThemeColor from '@/composables/useThemeColor';
 
 const PrimeVue = usePrimeVue();
 const { themesLight, currentTheme, changeThemeCurrent } = useThemeColor();
 
+const isDark = ref<boolean>(currentTheme.value.isDark);
+
 const toggleTheme = (newThemeName: string) => {
   PrimeVue.changeTheme(currentTheme.value.name, newThemeName, 'theme-link', () => {
     changeThemeCurrent(newThemeName);
   });
 };
+
+watch(currentTheme, (newValue) => {
+  isDark.value = newValue.isDark;
+});
 </script>
 
 <template>
   <section class="theme-selector">
     <span class="title">Colores</span>
+    <Tag class="mb-3"
+      :value="currentTheme.name"
+      :title="currentTheme.name"
+    />
     <div class="theme-row">
-      <Tag 
+      <Button 
         v-for="(theme, index) in themesLight" 
         :key="index" 
         @click="toggleTheme(theme.name)" 
         :style="{ backgroundColor: theme.color }"
         :value="theme.name"
         :title="theme.name"
+        :class="{ 'selected': currentTheme.name === theme.name }"
       >
-      </Tag>
+      </Button>
     </div>
   </section>
 </template>
@@ -50,4 +62,21 @@ const toggleTheme = (newThemeName: string) => {
   gap: 10px; /* Espacio entre los botones */
 }
 
+.theme-tag {
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: transform 0.2s;
+}
+
+.theme-tag.selected {
+  border: 2px solid #000; /* Ajusta el estilo para indicar el tema seleccionado */
+  transform: scale(1.1); /* Ajuste visual para resaltar el tema seleccionado */
+}
 </style>

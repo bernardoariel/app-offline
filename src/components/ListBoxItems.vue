b88065f4339e83b58169798ba9041a7ebc1cf9e3
GabrielNavas98 <cgnavas98@gmail.com>
2024-07-12 12:18:01 -0300

    fix tarjeta de efectos

--- src/components/ListBoxItems.vue
+++ src/components/ListBoxItems.vue
@@ -1,129 +1,175 @@
 <script setup lang="ts">
 //listBoxItems
 
-import { getUpperCase, getTitleCase } from "@/helpers/stringUtils";
+import { getUpperCase, getTitleCase } from '@/helpers/stringUtils';
 import { getColorByAfectado } from '@/helpers/getColorByAfectado';
+import { getColorByEstado } from '@/helpers/getColorByEstado';
 
 import { formatFecha } from '@/helpers/getFormatFecha';
 import useItemsComputados from '@/composables/useItemsComputados';
 import useItemValue from '@/composables/useItemValue';
 import useFieldState from '@/composables/useFiledsState';
 
-
-const { itemsComputados, routeType ,eliminarItem} = useItemsComputados();
-const { selectedItem } = useItemValue()
+const { itemsComputados, routeType, eliminarItem } = useItemsComputados();
+const { selectedItem } = useItemValue();
 const { statesID } = useFieldState();
-
 
 const getPristineById = (id: string) => {
   const found = statesID.find((state) => state.id === id);
   return found ? found.pristine : false;
 };
-
-
 </script>
 <template>
   <div class="card flex flex-column justify-content-center">
-    <Listbox  v-if="itemsComputados && itemsComputados.length > 0"
+    <Listbox
+      v-if="itemsComputados && itemsComputados.length > 0"
       v-model="selectedItem"
       :options="itemsComputados"
       optionLabel="nombre"
-      class="w-full listbox-lower">
-
+      class="w-full listbox-lower"
+    >
       <template #option="{ option }">
-        
-
         <div class="listbox-item fixed-height">
-
           <div class="left-column">
             <!-- Afectados y vinculados -->
             <div v-if="routeType === 'afectados' || routeType === 'vinculados'">
               <div class="text-row">
-                <span class="font-bold">{{ option.apellido ? getUpperCase(option.apellido) + ',' : '' }}</span>
-                <span class="ml-2">{{ option.nombre ? getTitleCase(option.nombre) : '' }}</span>
+                <span class="font-bold">{{
+                  option.apellido ? getUpperCase(option.apellido) + ',' : ''
+                }}</span>
+                <span class="ml-2">{{
+                  option.nombre ? getTitleCase(option.nombre) : ''
+                }}</span>
                 <span class="ml-5">
                   <i>{{ option.typeDocumento + ': ' }}</i>
                   <i>{{ option.nroDocumento }}</i>
                 </span>
-                       
               </div>
               <div class="tag-row">
-                <Tag :value="option.typeAfectado" :severity="getColorByAfectado(option.typeAfectado)" />
+                <Tag
+                  :value="option.typeAfectado"
+                  :severity="getColorByAfectado(option.typeAfectado)"
+                />
               </div>
             </div>
 
             <!-- fecha -->
             <div v-if="option && routeType === 'fecha'">
               <div class="text-row">
-                <span class="font-bold">Entre </span><span>{{ option.desdeFechaHora? formatFecha(option.desdeFechaHora): '' }} y {{ option.desdeFechaHora? formatFecha(option.hastaFechaHora):'' }}</span>
+                <span class="font-bold">Entre </span
+                ><span
+                  >{{
+                    option.desdeFechaHora
+                      ? formatFecha(option.desdeFechaHora)
+                      : ''
+                  }}
+                  y
+                  {{
+                    option.desdeFechaHora
+                      ? formatFecha(option.hastaFechaHora)
+                      : ''
+                  }}</span
+                >
               </div>
               <div class="tag-row">
-                <Tag :value="option.departamento" :severity="getColorByAfectado(option.departamento)" />
+                <Tag
+                  :value="option.departamento"
+                  :severity="getColorByAfectado(option.departamento)"
+                />
               </div>
             </div>
 
             <!-- personal interviniente -->
             <div v-else-if="routeType === 'personalInterviniente'">
               <div class="text-row">
-                <span class="font-bold">{{ option.apellido ? getUpperCase(option.apellido) + ',' : '' }}</span>
-                <span class="ml-2">{{ option.nombre ? getTitleCase(option.nombre) : '-' }}</span>
+                <span class="font-bold">{{
+                  option.apellido ? getUpperCase(option.apellido) + ',' : ''
+                }}</span>
+                <span class="ml-2">{{
+                  option.nombre ? getTitleCase(option.nombre) : '-'
+                }}</span>
               </div>
               <div class="tag-row">
-                <Tag :value="option.jerarquia" :severity="getColorByAfectado(option.jerarquia)" />
+                <Tag
+                  :value="option.jerarquia"
+                  :severity="getColorByAfectado(option.jerarquia)"
+                />
               </div>
               <div class="w-full" style="margin-top: -30px; margin-left: 55px">
-                  <p class="ml-5 text-xs">{{ option.dependencia }}</p>
+                <p class="ml-5 text-xs">{{ option.dependencia }}</p>
               </div>
             </div>
 
+            <!-- Efectos -->
             <div v-else-if="routeType === 'efectos'">
               <div class="text-row">
-                <span class="font-bold">{{ option.subcategoria ? getUpperCase(option.subcategoria) + ' ' : '' }}</span>
-                <span class="font-bold">{{ option.categoria ? getUpperCase(option.categoria) + ',' : '' }}</span>
-                <span class="ml-2">{{ option.marca ? getTitleCase(option.marca) : '-' }}</span>
-                <span class="ml-2">{{ option.modelo ? getTitleCase(option.modelo) : '-' }}</span>
-                <br>
-                <span>{{ option.tipo ? getTitleCase(option.tipo) : '-' }}</span>
+                <span class="font-bold">{{
+                  option.subcategoria
+                    ? getUpperCase(option.subcategoria) + ' '
+                    : ''
+                }}</span>
+                <span class="font-bold">{{
+                  option.categoria ? getUpperCase(option.categoria) + ',' : ''
+                }}</span>
+                <span class="ml-2">{{
+                  option.marca ? getTitleCase(option.marca) : '-'
+                }}</span>
+                <span class="ml-2">{{
+                  option.modelo ? getTitleCase(option.modelo) : '-'
+                }}</span>
+                <span
+                  >, {{ option.tipo ? getTitleCase(option.tipo) : '-' }}</span
+                >
+                <div class="tag-row">
+                  <Tag
+                    :value="option.estado ? getTitleCase(option.estado) : '-'"
+                    :severity="getColorByEstado(option.estado)"
+                  />
+                </div>
               </div>
-             
               <div class="w-full" style="margin-top: -30px; margin-left: 55px">
-                  <p class="ml-5 text-xs">{{ option.dependencia }}</p>
+                <p class="ml-5 text-xs">{{ option.dependencia }}</p>
               </div>
             </div>
-          
           </div>
 
           <div class="right-column">
             <!-- <Button icon="pi pi-trash" severity="danger" @click="eliminarItem(option.id)" /> -->
-            <Button v-if="option && option.code === 'new-item'" 
-              icon="pi pi-plus" 
-              rounded 
-              aria-label="Agregar" 
-              outlined 
-              severity="primary" />
-          <div class="button-and-dot-container" v-else-if="selectedItem === option && option.id">
-            
-            <div v-if="!getPristineById(option.id)" class="uncommited-dot bg-blue-400"></div>
-            <Button icon="pi pi-trash" 
-              severity="danger" 
-              @click="eliminarItem(option.id)" />
-          </div>
-          <div class="button-and-dot-container" v-else >
-          <div class="uncommited-dot bg-blue-400" v-if="!getPristineById(option.id)"></div>
-            <Button 
-                icon="pi pi-trash" 
-                severity="danger" 
-                disabled 
+            <Button
+              v-if="option && option.code === 'new-item'"
+              icon="pi pi-plus"
+              rounded
+              aria-label="Agregar"
+              outlined
+              severity="primary"
             />
-          </div>
+            <div
+              class="button-and-dot-container"
+              v-else-if="selectedItem === option && option.id"
+            >
+              <div
+                v-if="!getPristineById(option.id)"
+                class="uncommited-dot bg-blue-400"
+              ></div>
+              <Button
+                icon="pi pi-trash"
+                severity="danger"
+                @click="eliminarItem(option.id)"
+              />
+            </div>
+            <div class="button-and-dot-container" v-else>
+              <div
+                class="uncommited-dot bg-blue-400"
+                v-if="!getPristineById(option.id)"
+              ></div>
+              <Button icon="pi pi-trash" severity="danger" disabled />
+            </div>
           </div>
         </div>
       </template>
     </Listbox>
-    
   </div>
 </template>
-
 
 <style scoped>
 .fixed-height {
@@ -159,8 +205,7 @@ const getPristineById = (id: string) => {
   height: 12px; /* Tamaño del círculo */
   /* Color del círculo */
   border-radius: 50%; /* Hace que sea redondo */
-  margin-right:15px; /* Espacio entre el botón y el círculo */
-  
+  margin-right: 15px; /* Espacio entre el botón y el círculo */
 }
 .tag-row {
   margin-top: 4px; /* Espacio arriba del tag */

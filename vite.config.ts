2642030d8eaf258e6cd82dd5b74010ec76625dc3
GabrielNavas98 <cgnavas98@gmail.com>
2024-08-19 14:31:33 -0300

    reestructuracion carpetas

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,31 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import commonjs from '@rollup/plugin-commonjs';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   plugins: [
     vue(),
-    commonjs(), 
+    commonjs(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/primeicons/fonts/*',
+          dest: 'builded-app-offline/assets/fonts'
+        }, {
+          src: 'node_modules/primeicons/primeicons.css',
+          dest: 'builded-app-offline/assets'
+        }
+      ]
+    }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          injectLink: '<link rel="stylesheet" crossorigin href="/assets/css/primeicons.css">',
+        },
+      },
+    })
   ],
   resolve: {
     alias: {
@@ -16,12 +36,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
+        entryFileNames: 'builded-app-offline/assets/[name].js',
+        chunkFileNames: 'builded-app-offline/assets/[name].js',
+        assetFileNames: 'builded-app-offline/assets/[name].[ext]',
       },
     },
-    minify: false, 
+    minify: false,
   },
   optimizeDeps: {
     include: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts'],

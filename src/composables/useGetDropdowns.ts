96940e6af019d1ef76ff62686eae360a2de695c2
GabrielNavas <cgnavas98@gmail.com>
2024-09-04 15:04:59 -0300

    se agrego peticiones de efectos y fix de algunos endpoints

--- src/composables/useGetDropdowns.ts
+++ src/composables/useGetDropdowns.ts
@@ -1,78 +1,115 @@
-import { ref } from "vue"
-import { useFetch } from "./useFetch"
-import { sitios, modusOperandi, causaCaratula, juzgadoInterviniente, articulosRelacionados, ayudanteFiscal, fiscalCargo, ufiNro, delitos } from '@/data/actuacionNew'
+import { ref } from "vue";
+import { useFetch } from "./useFetch";
+import {
+    sitios,
+    modusOperandi,
+    causaCaratula,
+    juzgadoInterviniente,
+    articulosRelacionados,
+    ayudanteFiscal,
+    fiscalCargo,
+    ufiNro,
+    delitos,
+    categorias,
+    subcategorias,
+    modelosCategorias,
+    marcasCategorias,
+} from "@/data/actuacionNew";
+import { addNameProp } from "@/helpers/dropUtils";
 
-const hardcodedData = { sitios, modusOperandi, causaCaratula, juzgadoInterviniente, articulosRelacionados, ayudanteFiscal, fiscalCargo, ufiNro, delitos }
+const hardcodedData = {
+    sitios,
+    modusOperandi,
+    causaCaratula,
+    juzgadoInterviniente,
+    articulosRelacionados,
+    ayudanteFiscal,
+    fiscalCargo,
+    ufiNro,
+    delitos,
+    categorias,
+    subcategorias,
+    modelosCategorias,
+    marcasCategorias,
+};
 
-// console.log(process.env.VUE_APP_BACKEND_ENVIRONMENT)
 
-// const environment = import.meta.env.VITE_ENV;
-// const apiUrl = (() => {
-//     switch (environment) {
-//         case 'production':
-//             return import.meta.env.VITE_API_SW_PRODUCTION;
-//         case 'testing':
-//             return import.meta.env.VITE_API_SW_TESTING;
-//         case 'development':
-//         default:
-//             return import.meta.env.VITE_API_SW_DEVELOP;
-//     }
-// })();
+const apiUrl = "http://localhost/siis/api/v1";
 
-const apiUrl = 'http://localhost/siis/api/v1'
+// const apiUrl = 'http://localhost:3000'
 
 const customMap: { [key: string]: keyof typeof hardcodedData } = {
     'tipo-sitio': 'sitios',
     'tipo-modus-operandi': 'modusOperandi',
-
     'Causa Caratula': 'causaCaratula',
     'Delito': 'delitos',
-    
-    'articulos': 'articulosRelacionados',
-    'juzgado': 'juzgadoInterviniente',
-    'tipo-ufi': 'ufiNro',
-    
-    'ayudante': 'ayudanteFiscal',
-    'fiscal': 'fiscalCargo',
+    'articulos': "articulosRelacionados",
+    'juzgado': "juzgadoInterviniente",
+    'tipoufi': "ufiNro",
+    'ayudante': "ayudanteFiscal",
+    'fiscal': "fiscalCargo", 
+    //efectos
+    'categorias': "categorias",
+    "sub-categorias": "subcategorias",
+    'marcas': "marcasCategorias",
+    'modelos': "modelosCategorias",
 };
 
-function getData(name: string, search?:string) {
-    if(search){
+function getData(name: string, search?: string) {
+    if (search) {
         const key = customMap[search];
         return hardcodedData[key];
     }
     const key = customMap[name];
     return hardcodedData[key];
-
 }
 
-export async function useGetDropdowns (param: string, search?: string) {
+export async function useGetDropdowns(param: string, search?: string) {
     const dropdownData = ref<any>(null);
+
     const { data, fetchData } = useFetch<any>();
 
     const url = (() => {
         switch (param) {
-            case 'tipo-causa-caratula':
-                return `${apiUrl}/parametros/${param}/?search={"TipoCaratula": [{"operator": "=", "value": "${search}"}]}`
-            case 'personalfiscal':
-                return `${apiUrl}/${param}/?search={"Jerarquia": [{"operator": "=", "value": "${search}"}]}`
-            case 'tipo-modus-operandi':
-                return `${apiUrl}/parametros/${param}`
-            default:
+            case "tipo-causa-caratula":
+                return `${apiUrl}/parametros/${param}/?search={"TipoCaratula": [{"operator": "=", "value": "${search}"}]}`;
+            case "personalfiscal":
+                return `${apiUrl}/${param}?search={"Jerarquia": [{"operator": "=", "value": "${search}"}]}`;
+            case "tipo-modus-operandi":
+            case "categorias":
+            case "modelos":
+            case "sub-categorias":
+                return `${apiUrl}/parametros/${param}`;
+            case "tipo-sitio":
+            case "articulos":
                 return `${apiUrl}/${param}`;
+            case "marcas":
+                return `${apiUrl}/parametros/${param}/`;
+            default:
+                return `${apiUrl}/${param}/`;
         }
-    })()
+    })();
 
+    try {
+        await fetchData(url as string);
 
-    try{
-        await fetchData(url as string)
-        dropdownData.value = data.value || getData(param, search)
-    }catch(error){
+        if (data.value) {
+            if (param === "tipoufi") {
+                dropdownData.value = addNameProp(data.value.data, "Numero");
+            } else if (param === "personalfiscal") {
+                dropdownData.value = addNameProp(data.value.data, "Denominacion");
+            } else {
+                dropdownData.value = data.value.data;
+            }
+        } else {
+            dropdownData.value = getData(param, search);
+        }
+    } catch (error) {
         console.log("Error fetching data from API, using hardcoded data.", error);
-        dropdownData.value = hardcodedData;
-    } 
-    
-    return{
-        dropdownData
+        dropdownData.value = getData(param, search);
     }
+
+    return {
+        dropdownData,
+    };
 }
--- src/helpers/getDropItems.ts
+++ src/helpers/getDropItems.ts
@@ -3,10 +3,10 @@ import { mapToDropDownEfectos, mapToDropdownItems } from './dropUtils';
 import { departamentosDropdown } from "@/data/municipios";
 import { dependencias } from "@/data/dependencia";
 import { useGetDropdowns } from "@/composables/useGetDropdowns";
-import { categorias, subcategorias, tipoCategorias, marcasCategorias, modelosCategorias, ayudanteFiscal, delitos, sexo, documentos, afectados, nacionalidad, estadoCivil, instruccion, jerarquia, typesHasEstudies, typesShowDocument} from '@/data/actuacionNew'
+import { sexo, documentos, afectados, nacionalidad, estadoCivil, instruccion, jerarquia, typesHasEstudies, typesShowDocument } from '@/data/actuacionNew'
 
-const getItems = async (dropdownName:string, searchParam?:string) => {
-    const { dropdownData: data } =  await useGetDropdowns(dropdownName, searchParam)
+const getItems = async (dropdownName: string, searchParam?: string) => {
+    const { dropdownData: data } = await useGetDropdowns(dropdownName, searchParam)
     return data.value
 }
 
@@ -21,21 +21,19 @@ export const jerarquiaDropdown = ref(mapToDropdownItems(jerarquia))
 export const dependenciaDropdown = ref(mapToDropdownItems(dependencias))
 export const showDocumentDropdown = ref(mapToDropdownItems(typesShowDocument))
 export const hasEstudiesDropdown = ref(mapToDropdownItems(typesHasEstudies))
-export const categoriasDropdown = ref(mapToDropDownEfectos(categorias, "categorias"))
-export const subcategoriasDropdown = ref(mapToDropDownEfectos(subcategorias, "sub-categorias"))
-export const tipoCategoriasDropdown = ref(mapToDropDownEfectos(tipoCategorias, "tipo-categorias"))
-export const marcasCategoriasDropdown = ref(mapToDropDownEfectos(marcasCategorias, "marcas"))
-export const modelosCategoriasDropdown = ref(mapToDropDownEfectos(modelosCategorias, "modelo"))
+
+export const categoriasDropdown = ref(mapToDropDownEfectos(await getItems('categorias'), "categorias"))
+export const subcategoriasDropdown = ref(mapToDropDownEfectos(await getItems('sub-categorias'), "sub-categorias"))
+export const tipoCategoriasDropdown = ref(mapToDropDownEfectos(await getItems('categorias'), "tipo-categorias"))
+export const marcasCategoriasDropdown = ref(mapToDropDownEfectos(await getItems('marcas'), "marcas"))
+export const modelosCategoriasDropdown = ref(mapToDropDownEfectos(await getItems('modelos'), "modelo"))
+
 export const sitiosDropdwown = ref(mapToDropDownEfectos(await getItems('tipo-sitio')))
 export const modusOperandiDropdwown = ref(mapToDropDownEfectos(await getItems('tipo-modus-operandi')))
-
 export const causaCaratulaDropdwown = ref(mapToDropDownEfectos(await getItems('tipo-causa-caratula', 'Causa Caratula')))
 export const delitosDropdown = ref(mapToDropDownEfectos(await getItems('tipo-causa-caratula', 'Delito')))
-
 export const articulosDropdwown = ref(mapToDropDownEfectos(await getItems('articulos')))
 export const juzgadoIntervinienteDropdwown = ref(mapToDropDownEfectos(await getItems('juzgado')))
-
-export const ufiNroDropdown = ref(mapToDropDownEfectos(await getItems('tipo-ufi')))
-
+export const ufiNroDropdown = ref(mapToDropDownEfectos(await getItems('tipoufi')))
 export const ayudanteFiscalDropdown = ref(mapToDropDownEfectos(await getItems('personalfiscal', 'ayudante')))
 export const fiscalCargoDropdown = ref(mapToDropDownEfectos(await getItems('personalfiscal', 'fiscal')))

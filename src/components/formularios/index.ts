03ca1f02eb516492701733cd42506e9a70ce35e5
GabrielNavas <cgnavas98@gmail.com>
2024-10-16 15:26:08 -0300

    EfectosForm

--- src/components/formularios/index.ts
+++ src/components/formularios/index.ts
@@ -1,2 +1,3 @@
 export { default as AfectadosForm } from "./AfectadosForm.vue";
 export { default as VinculadosForm } from "./VinculadosForm.vue";
+export { default as EfectosForm } from "./EfectosForm.vue";

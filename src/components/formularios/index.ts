3c01564b0735acf74816e40c321fa42c2fb43844
GabrielNavas <cgnavas98@gmail.com>
2024-10-21 13:25:11 -0300

    personalInterviniente

--- src/components/formularios/index.ts
+++ src/components/formularios/index.ts
@@ -1,3 +1,4 @@
 export { default as AfectadosForm } from "./AfectadosForm.vue";
 export { default as VinculadosForm } from "./VinculadosForm.vue";
 export { default as EfectosForm } from "./EfectosForm.vue";
+export { default as PersonalIntervinienteComponent } from "./PersonalIntervinienteComponent.vue";

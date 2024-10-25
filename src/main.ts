981aa74e811caad03a0708ffe1cee73756a70ccf
GabrielNavas <cgnavas98@gmail.com>
2024-10-25 14:39:00 -0300

    calendario en español

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,54 @@ import { useStorageData } from "./composables/useStorageData";
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  locale: {
+    firstDayOfWeek: 1,
+    dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+    today: "Hoy",
+    clear: "Limpiar",
+    dateFormat: "dd/mm/yy",
+    weekHeader: "Sem",
+    monthNamesShort: [
+      "ene",
+      "feb",
+      "mar",
+      "abr",
+      "may",
+      "jun",
+      "jul",
+      "ago",
+      "sep",
+      "oct",
+      "nov",
+      "dic",
+    ],
+    dayNames: [
+      "domingo",
+      "lunes",
+      "martes",
+      "miércoles",
+      "jueves",
+      "viernes",
+      "sábado",
+    ],
+    dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+    monthNames: [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+    ],
+  },
+});
 app.use(ToastService);
 
 app.use(router);
